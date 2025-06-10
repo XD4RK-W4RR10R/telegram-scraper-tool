b8959ba552f7fe90ac080304253c61228fd3e3b6
XD Jibon Mahmud <130215528+XD4RK-W4RR10R@users.noreply.github.com>
2025-06-10 13:00:11 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+API_ID = 1234567                   # তোমার Telegram API ID (int)
+API_HASH = 'your_api_hash_here'   # তোমার Telegram API HASH (string)
+SESSION_NAME = 'session'           # session ফাইলের নাম (string)
+TARGET_GROUP = 'https://t.me/yourgroup'     # যেখান থেকে scrape বা add করবে, link বা username
+TARGET_CHANNEL = 'https://t.me/yourchannel' # invite.py জন্য channel link বা username
